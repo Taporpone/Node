97cb465cc69188db900e229c8c72ec9fe41c2ee9
Taporpone <taporpone@gmail.com>
2017-02-01 14:42:28 +0100

    1_4 final

---
+++ 1_4/OSinfo.js
@@ -0,0 +1,24 @@
+var os = require('os');
+var timeConverter = require('./TimeConverter.js');
+
+function getOSinfo(){
+  var type = os.type();
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var userInfo = os.userInfo();
+
+  if (type === 'Darwin'){
+    type = "OSX";
+  } else if (type === 'Windows_NT'){
+    type = "Windows";
+  }
+  console.log('System: ', type);
+  console.log('Release: ', release);
+  console.log('CPU: ', cpu);
+  console.log('Uptime: ' + timeConverter.timeConverter(uptime))
+  console.log('Username: ',userInfo.username);
+  console.log('Homedir: ',userInfo.homedir);
+}
+
+exports.print = getOSinfo;
