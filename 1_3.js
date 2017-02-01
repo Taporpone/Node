43d87b29c2371482050125d2b730921302cb126e
Taporpone <taporpone@gmail.com>
2017-02-01 21:20:37 +0100

    Updated

--- 1_3.js
+++ 1_3.js
@@ -6,30 +6,26 @@ process.stdin.on('readable',function(){
   var input = process.stdin.read();
   if (input !== null) {
     var instruction = input.toString().trim();
-
     switch (instruction){
       case '/exit':
         process.stdout.write('Quitting app!\n');
-        process.exit(); //quit anyway so no need for break;
-
+        process.exit();
       case '/lang':
-        var language = process.env.LANG; //defined for validation
-        if (language === undefined){
+        var language = process.env.LANG;
+        if (!language){
           process.stderr.write("Language isn't defined :(. Google around I guess...\n")
         }else{
           process.stdout.write('Language: ' + language + '\n');
         }
         break;
-
       case '/ver':
         var version = process.versions.node;
-        if (version === undefined){
+        if (!version){
           process.stderr.write("Thats strange since you are running this script using node, BUT I can't read node's version O.o\n");
         }else{
           process.stdout.write('Node version: ' + version + '\n')
         }
         break;
-
       default:
         process.stdout.write('Wrong instruction!\n');
     }
