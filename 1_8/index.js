99a02a09b318090ee36eef18f15f7048bae74987
Taporpone <taporpone@gmail.com>
2017-02-12 14:59:43 +0100

    Added index.html 404 handler

--- 1_8/index.js
+++ 1_8/index.js
@@ -2,23 +2,33 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request,response){
-  if (request.url == "/"){
+  function notFound(file){
+    response.writeHeader(404,{"Content-Type":"text/html; charset=utf-8"});
+    response.write('<h1>404</h1>');
+    response.end();
+    console.log(file + ' not found, serving static text!');
+  }
+
+  function serveContent(code,data,contentType){
+    response.writeHeader(code,{"Content-Type":contentType});
+    response.write(data);
+    response.end();
+    }
+
+  if (request.url === "/"){
     fs.readFile('index.html',function(err,data){
-      response.writeHeader(200,{"Content-Type":"text/html; charset=utf-8"});
-      response.write(data);
-      response.end();
+      if (err){
+        notFound('index.html');
+      }else{
+        serveContent(200,data,"text/html; charset=utf-8");
+      }
     });
   }else{
     fs.readFile('404.jpg',function(err,data){
       if (err){
-        response.writeHeader(404,{"Content-Type":"text/html; charset=utf-8"});
-        response.write('<h1>404</h1>');
-        response.end();
-        console.log('Image 404.jpg not found, serving static text!');
+        notFound('404.jpg');
       }else{
-        response.writeHeader(404,{"Content-Type":"image/jpeg; charset=utf-8"});
-        response.write(data);
-        response.end();
+        serveContent(404,data,"image/jpeg; charset=utf-8");
       }
     });
   }
